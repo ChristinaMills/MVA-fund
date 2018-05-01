a1358cbb54e05e8c0a8f2c239d37afa07cc396d0
Christina Mills <chrimills@gmail.com>
2018-05-01 13:29:36 -0700

    scroll function added

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-document.querySelector("#submit-button").addEventListener("click", function(event) {
-  event.preventDefault();
-}, false);
+// document.querySelector("#submit-button").addEventListener("click", function(event) {
+//   event.preventDefault();
+// }, false);
+
+// (function(){
+//   var timeout;
+//   var $window = $(window);
+//   var $navbar = $('.navbar');
+  
+//   $window.on('scroll', function(e){
+//     clearTimeout(timeout);
+//     console.log("working")
+//     timeout = setTimeout(function(){
+//       if ($window.scrollTop() < 6650) {
+//         $navbar.removeClass('hide');
+//         console.log("remove class working")
+//       } else {
+//         $navbar.addClass('hide');
+//         console.log("add class working")
+
+//       }
+//     }, 100);
+//   });
+// }());
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 500) {
+    // $('.bottomMenu').fadeIn();
+    console.log("in the if")
+  } else {
+    // $('.bottomMenu').fadeOut();
+    console.log("in the else")
+  }
+});
