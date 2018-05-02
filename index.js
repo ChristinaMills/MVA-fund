0039fb87fd16e5a8982d554414ef4691f177bfda
Christina Mills <chrimills@gmail.com>
2018-05-02 11:47:40 -0700

    sample photo

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 $(document).scroll(function() {
   var y = $(this).scrollTop();
   if (y > 500) {
-    $('.navbar').css('background-color', 'rgba(137, 194, 153, 0.961)');
+    $('.navbar').css('background-color', 'rgba(255,255,255, 0.9)');
+    $('.tab > a').css('color', 'black')
+
     console.log('color should change');
   } 
   else if (y < 500) {
     $('.navbar').css({'background-color' : '', 'opacity': '1'});
+    $('.tab > a').css('color', 'white')
   }
 });
