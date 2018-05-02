67538071293e4b69783509742268e9357622a924
Christina Mills <chrimills@gmail.com>
2018-05-02 11:11:34 -0700

    nav opacity/bacjground transition added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 $(document).scroll(function() {
   var y = $(this).scrollTop();
   if (y > 500) {
-    // $('.bottomMenu').fadeIn();
-  } else {
-    // $('.bottomMenu').fadeOut();
+    $('.navbar').css('background-color', 'rgba(137, 194, 153, 0.961)');
+    console.log('color should change');
+  } 
+  else if (y < 500) {
+    $('.navbar').css({'background-color' : '', 'opacity': '1'});
   }
 });
