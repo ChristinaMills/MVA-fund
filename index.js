d2d570bf554058cb8c58452bd6e1a7802cf1398e
Christina Mills <chrimills@gmail.com>
2018-05-03 12:13:47 -0700

    color toggle nav fix

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ $(document).scroll(function() {
   var y = $(this).scrollTop();
   if (y < 500) {
     $('.navbar').css({'background-color' : '', 'opacity': '1'});
-    $('.menu > a').css('color', 'white');
+    $('.menu a').css('color', 'white');
     $('#nav-logo > img').css('height', '0em');
   } 
   else if (y > 500) {
     $('.navbar').css('background-color', 'rgba(255,255,255, 0.9)');
-    $('.menu > a').css('color', 'black');
+    $('.menu a').css('color', 'black');
     $('#nav-logo > img').css('height', '3.5em');
+    console.log("y more than 500, running")
   }
 });
