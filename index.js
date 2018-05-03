26321da48eacbff60df65d4b197b4f6740b728be
Christina Mills <chrimills@gmail.com>
2018-05-03 10:43:59 -0700

    nav bar aligned with logo, links, all effects, not font

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ $(document).scroll(function() {
   var y = $(this).scrollTop();
   if (y > 500) {
     $('.navbar').css('background-color', 'rgba(255,255,255, 0.9)');
-    $('.tab > a').css('color', 'black')
+    $('.menu > a').css('color', 'black')
 
     console.log('color should change');
   } 
   else if (y < 500) {
     $('.navbar').css({'background-color' : '', 'opacity': '1'});
-    $('.tab > a').css('color', 'white')
+    $('.menu > a').css('color', 'white')
   }
 });
