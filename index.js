0103255d1a18515c76d15ab8bfbc540594104a18
Christina Mills <chrimills@gmail.com>
2018-05-09 12:38:45 -0700

    logo burgandy added

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ $(document).scroll(function() {
   if (y < 700) {
     $('.navbar').css({'background-color' : '', 'opacity': '1'});
     $('.menu a').css('color', 'white');
-    $('#nav-logo > img').css('height', '0em');
-    $('#nav-logo2 > img').css('height', '3.5em');
+    $("#nav-logo > img").attr("src","public/images/burg-logo-blur.png");
+    $('#nav-logo > img').css('height', '3.5em');
+    
 
   } 
   else if (y > 700) {
     $('.navbar').css('background-color', 'rgba(255,255,255, 0.9)');
     $('.menu a').css('color', 'black');
-    $('#nav-logo2 > img').css('height', '0em');
+    $("#nav-logo > img").attr("src","public/images/navbar-logo.png");
     $('#nav-logo > img').css('height', '3.5em');
     console.log("y more than 500, running")
   }
