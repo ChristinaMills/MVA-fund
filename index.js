5eac2de3aa1c0469d4affbdac66c13641aee6bdd
Christina Mills <chrimills@gmail.com>
2018-05-09 12:21:01 -0700

    strategies photo link added, logo button from principles page fixed

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 
 $(document).scroll(function() {
   var y = $(this).scrollTop();
-  if (y < 500) {
+  if (y < 700) {
     $('.navbar').css({'background-color' : '', 'opacity': '1'});
     $('.menu a').css('color', 'white');
     $('#nav-logo > img').css('height', '0em');
+    $('#nav-logo2 > img').css('height', '3.5em');
+
   } 
-  else if (y > 500) {
+  else if (y > 700) {
     $('.navbar').css('background-color', 'rgba(255,255,255, 0.9)');
     $('.menu a').css('color', 'black');
+    $('#nav-logo2 > img').css('height', '0em');
     $('#nav-logo > img').css('height', '3.5em');
     console.log("y more than 500, running")
   }
